c16c984d16f97a40266f0e71d0bc77acaef47ba4
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-05-14 16:46:12 -0500

    Travis ci file added

--- server/server/app.js
+++ server/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const keys = require('./config/keys')
 
 mongoose.connection.openUri('mongodb://localhost:27017/tdg', (err, res) => {
     if(err) throw err
---
+++ server/server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUser: process.env.MONGO_USER,
+    mongoHost: process.env.MONGO_HOST,
+    mongoDatabase: process.env.MONGODATABASE,
+    mongoPassword: process.env.MONGOPASSWORD,
+    mongoPort: process.env.MONGOPORT
+}
