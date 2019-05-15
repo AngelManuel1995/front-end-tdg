aacbf07a73f46b968e54534167e1f1a8475f313d
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-05-15 00:27:50 -0500

    Api and databse connected

--- server/server/config/keys.js
+++ server/server/config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
     mongoDatabase: process.env.MONGO_INITDB_DATABASE,
     mongoRootusername: process.env.MONGO_INITDB_ROOT_USERNAME,
-    mongoPassword: process.env.MONGO_INITDB_ROOT_PASSWORD
+    mongoPassword: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    mongoHost: process.env.MONGO_HOST,
+    mongoPort: process.env.MONGO_PORT,
 }
--- server/server/index.js
+++ server/server/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
 
-mongoose.connection.openUri('mongodb://localhost:27017/tdg', (err, res) => {
+console.log(keys)
+
+mongoose.connection.openUri(`mongodb://${keys.mongoRootusername}:${keys.mongoPassword}@${keys.mongoHost}:${keys.mongoPort}/${keys.mongoDatabase}`, (err, res) => {
     if(err) throw err
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'Online')
 })
@@ -11,6 +13,8 @@ mongoose.connection.openUri('mongodb://localhost:27017/tdg', (err, res) => {
 let app = express()
 
 app.use(function(req, res, next) {
+	'mongodb://nostradatos:r8jvpa9ks@nostradatos.arus.com.co:27018/nostradatos'
+	
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS");
